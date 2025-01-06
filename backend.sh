e63d132a5791761057d1cc4e243d41c974d895a4
awssiddhi30 <raviaws0110@gmail.com>
2025-01-06 06:10:43 +0530

    backend file update

---
+++ backend.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+R="\[e31m"
+G="\[e32m"
+Y="\[e33m"
+N="\[e0m"
+
+logs_folder="/var/log/expense-logs"
+log_file=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+Log_file_name="$Log_folder/$Log_file-$Timestamp.log"
+
+Validate(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ...$R Failure $N"
+        exit 1
+    else
+        echo -e "$2 ...$G Success $N"
+    fi
+}
+
+Check_root(){
+    if [ UserID -ne 0]; then
+        echo -e " $R Error: You should have root access to run this script $N"
+        exit 1
+    fi
+}
+echo "Script is running at: $Timestamp" &>>$Log_file_name
+
+dnf module disable nodejs -y &>>$Log_file_name
+validate $? "disabling nodejs"
+
+dnf module enable nodejs:20 -y &>>$Log_file_name
+validate $? "enabling nodejs"
+
+dnf install nodejs -y &>>$Log_file_name
+validate  $? "installing nodejs"
+
+useradd expense &>>$Log_file_name
+validate $? "adding expense user"
+
+mkdir /app &>>$Log_file_name
+validate $? "making directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$Log_file_name
+validate $? "downloading application"
+
+cd /app 
+validate $? "creating app directory"
+
+unzip /tmp/backend.zip &>>$Log_file_name
+validate $? "unziping code"
+
+npm install &>>$Log_file_name
+validate $? "installing dependencies"
+
+cp backend.server/home/ec2-user//etc/systemd/system/backend.service
+
+dnf install mysql -y &>>$Log_file_name
+validate $? "installing mysql"
+
+mysql -h mysql.daws30.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$Log_file_name
+validate $? "setting transaction and tables"
+
+systemctl daemon-reload &>>$Log_file_name
+validate $? "daemon reload"
+
+systemctl start backend &>>$Log_file_name
+validate $? "starting backend"
+
+systemctl enable backend &>>$Log_file_name
+validate $? "enabling backend"
+
+systemctl restart backend &>>$Log_file_name
+validate $? "restarting backend"
